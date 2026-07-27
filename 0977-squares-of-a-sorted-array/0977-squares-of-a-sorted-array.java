6df09c7dd69676ef39132822a0e4923ef8eec0e9
rishabhhag <rishabhag73@gmail.com>
2026-07-28 02:00:49 +0530

    Time: 1 ms (100%), Space: 47.3 MB (53.85%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,71 +1,22 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        List<Integer> neg=new ArrayList<>();
+        int n = nums.length;
+        int[] result = new int[n]; 
         
-        List<Integer> pos=new ArrayList<>();
-
-    //seperate neg pos numbers
-    for (int num : nums){
-        if (num<0)
-        neg.add(num);
-        else
-        pos.add(num);
-    }
-
-    //case1 no negative values
-
-    if (neg.size() == 0){
-        for (int i=0; i<pos.size(); i++)
-            pos.set(i,pos.get(i)*pos.get(i));
-         return pos.stream().mapToInt(Integer:: intValue).toArray();
-
-    }
-    //Case2 no pos numbers
-    if (pos.size() == 0){
-        for (int i=0; i<neg.size(); i++)
-            neg.set(i,neg.get(i)*neg.get(i));
-        Collections.reverse(neg);    
-        return neg.stream().mapToInt(Integer:: intValue).toArray();
-    }
-    //case 3 both neg and pos exist
-
-    int i=0;
-    int j=0;
-    int id=0;
-    int n1=neg.size();
-    int n2=pos.size();
-    int[] res=new int[n1+n2];
-
-    for(i=0;i<n1;i++){
-        neg.set(i,neg.get(i)*neg.get(i));
-    }
-    Collections.reverse(neg);
-
-    
-    for(i=0;i<n2;i++)
-        pos.set(i,pos.get(i)*pos.get(i));
-    
-
-    
-    i=0;
-    j=0;
-    while(i<n1 && j <n2){
-        if(neg.get(i) <= pos.get(j)){
-            res[id++]=neg.get(i++);
-        }else{
-            res[id++]=pos.get(j++);
+        int left = 0;
+        int right = n - 1;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            // A single basic addition to determine which side is larger
+            if (nums[left] + nums[right] < 0) {
+                result[i] = nums[left] * nums[left];
+                left++; 
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--; 
+            }
         }
-    
-    }
-
-    while(i<n1)
-        res[id++]=neg.get(i++);
-
-    while(j<n2)
-        res[id++]=pos.get(j++);
-
-    return res;
-
+        
+        return result;
     }
 }
